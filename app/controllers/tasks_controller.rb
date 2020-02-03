cb054c068313b18128173111df4d464feca221b6
Hara-castle <minanma.harajo@gmail.com>
2020-02-03 12:58:03 +0000

    login

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,7 +1,10 @@
 class TasksController < ApplicationController
   before_action :set_task, only:[:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:index]
+  
   def index
-    @tasks = Task.all
+    @tasks = current_user.tasks #user_idで紐づいたものだけ表示
   end
 
   def show
@@ -12,8 +15,7 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)  #user_idに追加される
     if @task.save
       flash[:success] = 'Taskが正常に投稿されました'
       redirect_to @task
@@ -55,5 +57,12 @@ class TasksController < ApplicationController
   #Strong Parameter
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    #unless @task
+      #redirect_to root_url
+    #end
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,3 +1,4 @@
 class Task < ApplicationRecord
+    belongs_to :user  #ここいる
     validates :status, presence: true, length: { maximum: 10 }
 end
